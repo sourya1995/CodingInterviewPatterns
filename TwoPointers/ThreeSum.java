66e15fdd15b7b1eeeb1014a2b88761a6e620e2e8
Sourya Bhattacharya <37332817+sourya1995@users.noreply.github.com>
2023-07-05 15:30:01 +0530

    3 Sum

--- TwoPointers/ThreeSum.java
+++ TwoPointers/ThreeSum.java
@@ -1,3 +1,30 @@
+import java.util.Arrays;
+
 public class ThreeSum {
-    
+    public static boolean findSumOfThree(int nums[], int target){
+        Arrays.sort(nums);
+        int low, high, triples;
+
+        for(int i = 0; i < nums.length - 2; i++){
+            low = i + 1;
+            high = nums.length - 1;
+
+            while(low < high){
+                triples = nums[i] + nums[low] + nums[high];
+
+                if(triples == target){
+                    return true;
+                }
+
+                else if(triples < target){
+                    low++;
+                }
+
+                else {
+                    high--;
+                }
+            }
+        }
+        return false;
+    }
 }
