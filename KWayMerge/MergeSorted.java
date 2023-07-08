837521ce5bb183c17037a3347a5994b4adee39ea
Sourya Bhattacharya <37332817+sourya1995@users.noreply.github.com>
2023-07-08 15:56:24 +0530

    merge sorted array

--- KWayMerge/MergeSorted.java
+++ KWayMerge/MergeSorted.java
@@ -1,36 +1,25 @@
 package KWayMerge;
 
 public class MergeSorted {
-    public static String printArraywithMarkers(int[] arr, int pValue) {
-    String out = "[";
-    for (int i = 0; i < arr.length - 1; i++) {
-      if (i == pValue) {
-        out += "«";
-        out += String.valueOf(arr[i]) + "»" + ", ";
-      }
-      else {
-        out += String.valueOf(arr[i]) + ", ";
-      }
-    }
-    if (pValue == arr.length - 1) {
-      out += "«" + arr[arr.length - 1];
-      out += "»]";
-    }
-    else {
-      out += arr[arr.length - 1];
-      out += "]";
-    }
-    return out;
-  }
-  public static void mergeSorted(int[] nums1, int m, int[] nums2, int n) {
-    int p1 = m - 1; // set p1 to the last initialised element of nums1
-    int p2 = n - 1; // set p2 to the last element of nums2
-    System.out.println("\n\t\tInitialising p1 to the last data element of nums1:");
-    System.out.println("\t\t\t" + printArraywithMarkers(nums1, p1));
-    System.out.println("\t\t\tp1: " + p1 + ", value: " + nums1[p1]);
-    System.out.println("\n\t\tInitialising p2 to the last data element of nums2:");
-    System.out.println("\t\t\t" + printArraywithMarkers(nums2, p2));
-    System.out.println("\t\t\tp1: " + p2 + ", value: " + nums2[p2]);
 
-  }
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int p1 = m - 1;
+        int p2 = n - 1;
+        for (int p = m + n - 1; p >= 0; p--) {
+            if (p2 < 0) {
+                break;
+
+            }
+
+            if (p1 >= 0 && nums1[p1] > nums2[p2]) {
+                nums1[p] = nums1[p1];
+                p1 -= 1;
+            }
+
+            else {
+                nums1[p] = nums2[p2];
+                p2 -= 1;
+            }
+        }
+    }
 }
