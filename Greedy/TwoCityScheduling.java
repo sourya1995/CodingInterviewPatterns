8e7330571f7ba9f226e6e635697f86ab6ab065fa
Sourya Bhattacharya <37332817+sourya1995@users.noreply.github.com>
2023-07-09 12:55:43 +0530

    Two City Interview Scheduling

---
+++ Greedy/TwoCityScheduling.java
@@ -0,0 +1,18 @@
+package Greedy;
+
+import java.util.Arrays;
+
+public class TwoCityScheduling {
+
+    public int twoCitySchedCost(int[][] costs) {
+        int totalCost = 0;
+        Arrays.sort(costs, (a, b) -> (a[0] - a[1]) - (b[0] - b[1]));
+        int costLength = costs.length;
+
+        for (int i = 0; i < costLength / 2; i++) {
+            totalCost += costs[i][0] + costs[costLength - i - 1][1];
+        }
+
+        return totalCost;
+    }
+}
