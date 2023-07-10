492a0c44aa48b564a2c250c77a39f448c39f8736
Sourya Bhattacharya <37332817+sourya1995@users.noreply.github.com>
2023-07-10 16:09:42 +0530

    Basic Calculator

---
+++ Stacks/BasicCalculator.java
@@ -0,0 +1,45 @@
+package Stacks;
+
+import java.util.Stack;
+
+public class BasicCalculator {
+
+    public int calculate(String s) {
+        int signValue = 1;
+        int number = 0;
+        int result = 0;
+        int secondValue = 0;
+
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (Character.isDigit(c)) {
+                number = number * 10 + Character.getNumericValue(c);
+
+            } else if (c == '+' || c == '-') {
+                result += number * signValue;
+                if (c == '-')
+                    signValue = -1;
+                else
+                    signValue = 1;
+                number = 0;
+            } else if (c == '(') {
+                stack.push(result);
+                stack.push(signValue);
+                result = 0;
+                signValue = 1;
+            } else if (c == ')') {
+                result += signValue * number;
+                int popSignValue = stack.pop();
+                result *= popSignValue;
+
+                secondValue = stack.pop();
+                result += secondValue;
+                number = 0;
+            }
+
+        }
+
+        return result + number * signValue;
+    }
+}
