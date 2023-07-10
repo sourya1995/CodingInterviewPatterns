a65b363a82aae7c0cc329c46a4517425ab7866ea
Sourya Bhattacharya <37332817+sourya1995@users.noreply.github.com>
2023-07-10 16:57:49 +0530

    remove duplicates from string

---
+++ Stacks/RemoveDuplicates.java
@@ -0,0 +1,24 @@
+package Stacks;
+
+import java.util.Stack;
+
+public class RemoveDuplicates {
+
+    public String removeDuplicates(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (!stack.isEmpty() && stack.peek() == c) {
+                stack.pop();
+            } else {
+                stack.push(c);
+            }
+        }
+
+        StringBuilder result = new StringBuilder();
+        while (!stack.isEmpty()) {
+            result.insert(0, stack.pop());
+        }
+
+        return result.toString();
+    }
+}
