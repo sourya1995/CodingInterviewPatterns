0c442a07fb2030adce6727c2cbbeacd3caa62c6d
Sourya Bhattacharya <37332817+sourya1995@users.noreply.github.com>
2023-07-11 17:37:54 +0530

    Populating next level pointers

---
+++ TreeBFS/PopulateNextPointers.java
@@ -0,0 +1,56 @@
+package TreeBFS;
+
+import org.w3c.dom.Node;
+
+public class PopulateNextPointers {
+    /*
+     * // Definition for a Node.
+     * class Node {
+     * public int val;
+     * public Node left;
+     * public Node right;
+     * public Node next;
+     * 
+     * public Node() {}
+     * 
+     * public Node(int _val) {
+     * val = _val;
+     * }
+     * 
+     * public Node(int _val, Node _left, Node _right, Node _next) {
+     * val = _val;
+     * left = _left;
+     * right = _right;
+     * next = _next;
+     * }
+     * };
+     */
+
+    class Solution {
+        public Node connect(Node root) {
+            if (root == null)
+                return null;
+
+            Node levelStart = root;
+            while (levelStart != null) {
+                Node curr = levelStart;
+
+                while (curr != null) {
+                    if (curr.left != null) {
+                        curr.left.next = curr.right;
+                    }
+
+                    if (curr.right != null && curr.next != null) {
+                        curr.right.next = curr.next.left;
+                    }
+
+                    curr = curr.next;
+                }
+
+                levelStart = levelStart.left;
+            }
+
+            return root;
+        }
+    }
+}
