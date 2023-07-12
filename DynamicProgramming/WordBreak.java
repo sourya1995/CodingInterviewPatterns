8cf8549a344676c15c023fc1e357d37207530c4d
Sourya Bhattacharya <37332817+sourya1995@users.noreply.github.com>
2023-07-12 09:59:17 +0530

    Word Break

---
+++ DynamicProgramming/WordBreak.java
@@ -0,0 +1,26 @@
+package DynamicProgramming;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class WordBreak {
+    class Solution {
+        public boolean wordBreak(String s, List<String> wordDict) {
+            int n = s.length();
+            Set<String> wordSet = new HashSet<>(wordDict);
+            boolean[] dp = new boolean[n + 1];
+            dp[0] = true;
+            for (int i = 1; i <= n; i++) {
+                for (int j = 0; j < i; j++) {
+                    if (dp[j] && wordSet.contains(s.substring(j, i))) {
+                        dp[i] = true;
+                        break;
+                    }
+                }
+            }
+            return dp[n];
+
+        }
+    }
+}
