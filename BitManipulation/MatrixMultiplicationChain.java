78ea8bc7d6f324adeeec5b48e9069f4f969ed590
Sourya Bhattacharya <37332817+sourya1995@users.noreply.github.com>
2023-07-16 09:47:08 +0530

    Matrix Chain Multiplication

---
+++ BitManipulation/MatrixMultiplicationChain.java
@@ -0,0 +1,26 @@
+package BitManipulation;
+
+public class MatrixMultiplicationChain {
+    public static long minMultiplications(int[] dims){
+        long[][] dp = new long[dims.length][dims.length];
+        for(int i = 0; i < dims.length; i++){
+            for(int j = 0; j < dims.length; j++){
+                dp[i][j] = 0;
+            }
+        }
+
+        for(int l = 2; l < dims.length; l++){
+            for(int i = 1; i < ((dims.length) - l + 1); i++){
+                int j = i + l - 1;
+                dp[i][j] = Integer.MAX_VALUE;
+                for(int k = i; k < j; k++){
+                    long temp = dp[i][k] + dp[k+1][j] + dims[i - 1]* dims[k]* dims[j];
+                    if(temp < dp[i][j]){
+                        dp[i][j] = temp;
+                    }
+                }
+            }
+        }
+        return dp[1][dp.length - 1]; 
+    }
+}
