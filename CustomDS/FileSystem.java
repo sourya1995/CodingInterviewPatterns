abf4af38834c79945da40b427b27b9424d8b6972
Sourya Bhattacharya <37332817+sourya1995@users.noreply.github.com>
2023-07-21 09:28:06 +0530

    Design File System

--- CustomDS/FileSystem.java
+++ CustomDS/FileSystem.java
@@ -1,71 +1,42 @@
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
-import java.util.List;
+import java.util.Map;
+
+class TrieNode{
+    public Map<String, TrieNode> children = new HashMap<>();
+    public int value;
+    public TrieNode(int value){
+        this.value = value;
+    }
+}
 
 public class FileSystem {
-    class Dir{
-        HashMap<String, Dir> dirs = new HashMap<>();
-        HashMap<String, String> files = new HashMap<>();
-    }
-
-    Dir root;
-
-    public FileSystem(){
-        root = new Dir();
-    }
-
-    public List<String> ls(String path){
-        Dir t = root;
-        List<String> files = new ArrayList<>();
-        if(!path.equals("/")){
-            String[] d = path.split("/");
-            for (int i = 1; i < d.length - 1; i++) {
-                t = t.dirs.get(d[i]);
-            }
-
-            if(t.files.containsKey(d[d.length - 1])){
-                files.add(d[d.length - 1]);
-                return files;
-            } else {
-                t = t.dirs.get(d[d.length - 1]);
-            }
+    public boolean createPath(String path, int value){
+        final String[] subpaths = path.split("/");
+        TrieNode node = root;
+        for(int i=1; i<subpaths.length - 1; i++){
+            if(!node.children.containsKey(subpaths[i]))
+                return false;
+            node = node.children.get(subpaths[i]);
         }
 
-        files.addAll(new ArrayList<>(t.dirs.keySet()));
-        files.addAll(new ArrayList<>(t.files.keySet()));
-        Collections.sort(files);
-        return files;
+        final String lastSubpath = subpaths[subpaths.length - 1];
+        if(node.children.containsKey(lastSubpath))
+            return false;
+        node = node.children.put(lastSubpath, new TrieNode(value));
+        return true;
     }
 
-    public void mkdir(String path){
-        Dir t = root;
-        String[] d = path.split("/");
-        for (int i = 1; i < d.length; i++){
-            if(!t.dirs.containsKey(d[i])){
-                t.dirs.put(d[i], new Dir());
-            }
-
-            t = t.dirs.get(d[i]);
-        }
-    }
-
-    public void addContentToFile(String filePath, String content){
-        Dir t = root;
-        String[] d = filePath.split("/");
-        for(int i = 1; i < d.length - 1; i++){
-            t = t.dirs.get(d[i]);
-        }
-        t.files.put(d[d.length - 1], t.files.getOrDefault(d[d.length - 1], "") + content);
-    }
-
-    public String readContentFromFile(String filePath){
-        Dir t = root;
-        String[] d = filePath.split("/");
-        for(int i = 1; i < d.length - 1; i++){
-            t = t.dirs.get(d[i]);
+    public int get(String path){
+        final String[] subpaths = path.split("/");
+        TrieNode node = root;
+        for(int i = 1; i < subpaths.length; i++){
+            if(!node.children.containsKey(subpaths[i]))
+                return -1;
+            node = node.children.get(subpaths[i]);
         }
 
-        return t.files.get(d[d.length - 1]);
+        return node.value;
     }
+
+    private TrieNode root = new TrieNode(0);
 }
